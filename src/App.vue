f9d78caf37d848050c92c77ccd14bfa77c67bcb1
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-10-17 11:25:10 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app light>
+    <h1>Hello world!</h1>
+  </v-app>
+</template>
+
+<script>
+  export default {
+  }
+
+</script>
+
+<style lang="stylus">
+
+  @import './stylus/main'
+</style>
